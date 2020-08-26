1949e35c466170c58fabb5315395d2ef8531df15
lazyoracle <contact@anuragsaharoy.me>
2020-08-26 09:31:13 +0800

    update conf to sphinx-rtd-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../../"))
 
@@ -41,6 +42,7 @@ extensions = [
     # "sphinx.ext.autosummary",
     # Recursive documentation
     "autoapi.extension",
+    "sphinx_rtd_theme",
 ]
 
 autoapi_dirs = ["../../src"]
@@ -81,7 +83,7 @@ exclude_patterns = ["logs", "config"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
